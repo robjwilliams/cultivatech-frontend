0ecb2ebb9a9de9cbd74b28bbc22907a1ec0418e4
robjwilliams <robert151196@gmail.com>
2023-09-11 16:12:30 -0300

    Adds plant animation to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,19 @@
 // pages/index.tsx
 
 import Head from "next/head";
-import UserInfo from "./user-info";
-import LoginButton from "../components/LoginButton";
+import PlantAnimation from "../components/PlantAnimation";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>My Next.js Site</title>
+        <title>CultivaTech</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <main>
-        <h1>Welcome to My Next.js Site</h1>
-        <p>This is a simple landing page.</p>
-        <UserInfo />
-        <LoginButton />
+        <PlantAnimation />
       </main>
-
-      <footer>
-        <p>My Footer</p>
-      </footer>
-    </div>
+    </>
   );
 }
